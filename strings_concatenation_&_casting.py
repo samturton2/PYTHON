d842f495d64e02ad354dd1af56b3480f9189be3f
samturton2 <STurton@spartaglobal.com>
2020-11-02 17:00:16 +0000

    concatenations and casting notes added

--- strings_concatenation_&_casting.py
+++ strings_concatenation_&_casting.py
@@ -17,7 +17,39 @@ print(int(last_name))
 name = input("Please enter your name\n=>")
 print("Hello " + name)
 
+# Variables_task update
 # Get user data
 # Display message your age is +
 # Address - including postcode and first line of address - House number
 # DOB
+
+# Declaring strings with double and single quotes
+# "" as well as ''
+single_quotes = 'Single quotes \'wow\''
+print(single_quotes)
+double_quotes = "double quotes 'wow'"
+print(double_quotes)
+
+# String Slicing - why -
+# Indexing starts from 0
+greetings = "Hello World!"
+print(greetings[:5])
+# Can do negative indexing
+print(greetings[-6:])
+# Len returns the length of string variable
+print(len(greetings))
+
+white_spaces = "lot's of spaces at the end              "
+print(len(white_spaces))
+# Delete the white spaces at the end of the string
+print(len(white_spaces.strip()))
+
+# count() method counts the number a given word occurs in a string
+example_text = "lot's of text with some text"
+print(example_text.count('text'))
+# Temporary replace "test" with "words"
+print(example_text.replace("text", "words"))
+
+print(example_text.capitalize())
+print(example_text.upper())
+print(example_text.lower())
