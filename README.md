7559613f146333973ca432585acc550d6cdddc7d
samturton2 <STurton@spartaglobal.com>
2020-11-02 15:05:43 +0000

    added types of data to python variable

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# Python repository
-- This repo will hold all the python projects completed in SpartaGlobal Engineering74 course
-- Python notes will be held in Engineering 74 repo
+# Python
+## Pycharm setup
+### Python 3.9.0
 
+## Why learn Python
+- Supports multiple systems and platforms
+- Object orientated programming (OOP) driven
+- fastest growing language
+- being adopted by lots of teams (machine learning, data analyst, etc. 
+  - easy to read
+  - easy to debug
+  - faster creation of code
+  - faster comms between teams
+  - Improves productivity
+- extensive support libraries
+- allows easy scalability of complex applications
+
+## What is a variable
+- is a place holder to store the data
+
+## psuedo code
+- Describing code in comments for non technical readers
--- python_variables.py
+++ python_variables.py
@@ -1,1 +1,34 @@
-print('hello world')
+# How can we create a variable
+
+name = "Sam" # declaring string variable
+# creating variable called name to store user name
+age = 22 # integer
+
+hourly_wage = 10 # integer
+
+travel_allowance = 2.4 # float
+
+# Type of variables in python
+# String values, Int, Float and boolean
+
+print(name)
+print(travel_allowance)
+print(age)
+print(hourly_wage)
+
+# How can we take user data
+# We can use a method called input() to get data from user
+
+name = input("please enter your name ")
+# Getting user data through input() method
+print(name)
+# How can we find out the type of variable
+# We have the type method in python that gives us the datatype
+print(type(age))
+
+
+# Receive data from user
+input()
+# display data on command line
+print()
+# save data in csv file
