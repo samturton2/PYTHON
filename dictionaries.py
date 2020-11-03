604b905f43a6faa14b7a3d7105d4895d1edb488c
samturton2 <STurton@spartaglobal.com>
2020-11-03 14:52:31 +0000

    dictionaries finished off

--- dictionaries.py
+++ dictionaries.py
@@ -10,7 +10,8 @@ devops_student = {
     "name": "james",
     "stream": "tech",
     "completed_lessons": 4,
-    "completed_lesson_names": ["operators", "data types", "variables", "sql"]
+    "completed_lesson_names": ["operators", "data types", "variables", "sql"],
+    "Hobbies": ["watching movies", "rugby", "snowboarding"]
 }
 
 print(type(devops_student))
@@ -23,3 +24,22 @@ print(devops_student.keys())
 
 # How can we fetch the value called "data types"
 print(devops_student["completed_lesson_names"][1])
+
+# How can i change the value of specific key
+devops_student["completed_lessons"] = 3
+print(devops_student["completed_lessons"])
+print(type(devops_student.items()))
+
+#Task
+# create a New dictionary to store user details
+
+# all the details that you utilised in the last task name, DOB, Address, course, grades,
+# methods of dictionary to remove, add, replace, display the type of items
+# create a list of hobbies of at least 3 items
+# display data in reverse order of hobby list
+
+devops_student["Hobbies"].append("running")
+print(devops_student)
+
+devops_student["Hobbies"].remove("running")
+print(devops_student)
