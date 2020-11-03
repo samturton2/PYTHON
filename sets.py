c19a200ff1f94c939bcded307188a96a34f01510
samturton2 <STurton@spartaglobal.com>
2020-11-03 15:02:07 +0000

    sets file added

---
+++ sets.py
@@ -0,0 +1,15 @@
+# set - data collection with sets
+# indexing - THERE IS NO INDEXING IN SETS
+# sets are unordered
+# Syntax: {} brackets for sets
+
+car_parts = {"wheels", "doors", "radio"}
+print(car_parts)
+
+# managing data with sets
+# add an item to the sets
+car_parts.add("seat belts")
+print(car_parts)
+# remove an item from the set
+car_parts.remove("wheels")
+print(car_parts)
